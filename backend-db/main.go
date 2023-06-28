f5676b55cf720328460b2505c5021982e60303b1
ธนโชติ ศักดิ์ธรรมเจริญ <45006016+Pet002@users.noreply.github.com>
2023-06-28 14:05:48 +0000

    :hammer: Update struc

--- backend-db/main.go
+++ backend-db/main.go
@@ -30,7 +30,10 @@ func main() {
 	}()
 
 	var result bson.M
-	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Decode(&result); err != nil {
+	if err := client.Database("admin").RunCommand(context.TODO(), bson.D{{
+		Key: "ping",
+		Value: 1,
+	}}).Decode(&result); err != nil {
 		panic(err)
 	}
 	fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
